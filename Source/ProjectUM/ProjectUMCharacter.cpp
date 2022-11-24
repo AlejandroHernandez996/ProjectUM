c378cd7bd3d69f90a539976726429d81c44b2028
alejandrohernandez996 <alejandrohernandez996@gmail.com>
2022-11-23 18:11:20 -0600

    cool trees

--- Source/ProjectUM/ProjectUMCharacter.cpp
+++ Source/ProjectUM/ProjectUMCharacter.cpp
@@ -81,7 +81,7 @@ AProjectUMCharacter::AProjectUMCharacter()
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 500.0f; // The camera follows at this distance behind the character	
+	CameraBoom->TargetArmLength = 800.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
